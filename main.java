b6580bc6c12958d9cf1756685c6c7fa372cf65d1
EmiraDahilvis-blip <2510631250058@student.unsika.ac.id>
2026-05-16 22:56:46 +0700

    Co-authored-by: 2510631250001-hub <2510631250001-hub@users.noreply.github.com> Co-authored-by: hadidkurniafadillah-del <hadidkurniafadillah-del@users.noreply.github.com> Co-authored-by: ItzTyoKunz <ItzTyoKunz@users.noreply.github.com>

---
+++ main.java
@@ -0,0 +1,26 @@
+public static void main(String[] args) {
+    isiData();
+
+    while (true) {
+        System.out.println("Sistem Nilai IPK Mahasiswa");
+        System.out.println("1. Tambah Data");
+        System.out.println("2. Tampilkan Data");
+        System.out.println("3. Edit Data");
+        System.out.println("4. Hapus Data");
+        System.out.println("5. Cari Nama mahasiswa");
+        System.out.println("6. Cari NPM mahasiswa");
+        System.out.println("7. Cari Kategori Nilai mahasiswa");
+        System.out.println("8. Urutkan Data berdasarkan Nama");
+        System.out.println("9. Urutkan Data berdasarkan NPM");
+        System.out.println("10. Urutkan Data berdasarkan IPK");
+        System.out.println("11. Statistik");
+        System.oiut.println("12. Keluar");
+        System.out.println("Pilih menu (1-12):");
+
+        int pilih = input.nextInt();
+            input.nextLine(); 
+
+
+    }
+    
+}
