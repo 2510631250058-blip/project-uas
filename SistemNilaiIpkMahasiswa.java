df90ccfb0240438573502116b4bed2340c2892df
EmiraDahilvis-blip <2510631250058@student.unsika.ac.id>
2026-05-16 21:14:38 +0700

    tambah isiData

--- SistemNilaiIpkMahasiswa.java
+++ SistemNilaiIpkMahasiswa.java
@@ -5,4 +5,36 @@ public class SistemNilaiIpkMahasiswa {
     static ArrayList<mahasiswa> data = new ArrayList
     static Scanner input = new Scanner(System.in);
     
+    static void isiData() {
+
+        data.add(new Mahasiswa("250101001", "Mamat", 3.6));
+        data.add(new Mahasiswa("250101002", "Slamet", 3.2));
+        data.add(new Mahasiswa("250101003", "Citra", 3.8));
+        data.add(new Mahasiswa("250101004", "Dewi", 2.9));
+        data.add(new Mahasiswa("250101005", "Eka", 3.4));
+        data.add(new Mahasiswa("250101006", "Fajar", 3.7));
+        data.add(new Mahasiswa("250101007", "Gina", 3.1));
+        data.add(new Mahasiswa("250101008", "Hardi", 2.8));
+        data.add(new Mahasiswa("250101009", "Indah", 3.9));
+        data.add(new Mahasiswa("250101010", "Joko", 3.0));
+        data.add(new Mahasiswa("250101011", "Kiki", 3.5));
+        data.add(new Mahasiswa("250101012", "Lina", 3.3));
+        data.add(new Mahasiswa("250101013", "Maya", 2.7));
+        data.add(new Mahasiswa("250101014", "Nina", 3.6));
+        data.add(new Mahasiswa("250101015", "Oki", 3.2));
+        data.add(new Mahasiswa("250101016", "Putra", 3.8));
+        data.add(new Mahasiswa("250101017", "Qori", 3.1));
+        data.add(new Mahasiswa("250101018", "Rina", 2.9));
+        data.add(new Mahasiswa("250101019", "Santi", 3.7));
+        data.add(new Mahasiswa("250101020", "Tono", 3.4));
+        data.add(new Mahasiswa("250101021", "Umar", 3.0));
+        data.add(new Mahasiswa("250101022", "Vina", 3.6));
+        data.add(new Mahasiswa("250101023", "Wawan", 3.2));
+        data.add(new Mahasiswa("250101024", "Xena", 3.9));
+        data.add(new Mahasiswa("250101025", "Yani", 2.8));
+        data.add(new Mahasiswa("250101026", "Zaki", 3.5));
+        data.add(new Mahasiswa("250101027", "Alya", 3.3));
+        data.add(new Mahasiswa("250101028", "Bagas", 2.7));
+        data.add(new Mahasiswa("250101029", "Abdul", 3.8));
+        data.add(new Mahasiswa("250101030", "Yana", 3.1));
 }
