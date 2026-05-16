1aca911dcf112558a4daa002f930ffd1b43e32dd
EmiraDahilvis-blip <2510631250058@student.unsika.ac.id>
2026-05-16 20:57:24 +0700

    buat system niali ipk

---
+++ SistemNilaiIpkMahasiswa.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class SistemNilaiIpkMahasiswa {
+
+    static ArrayList<mahasiswa> data = new ArrayList
+    static Scanner input = new Scanner(System.in);
+    
+}
